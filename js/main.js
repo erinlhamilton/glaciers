24fbb1100f8eb3683f0e399cbbdcb9e09c795c42
Erin Hamilton <erin@erinhamilton.me>
2014-02-05 13:27:35 -0600

    Initial libraries and file structure

---
+++ js/main.js
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////////////////////////
+// Title:           Laurentide Ice Sheet Ice-Margin Positions
+//
+// Source Files:    jQuery.v1.9.1.min.js, jquery-ui-1.10.3.custom.min.js,
+//		    		map.js, timeline.js
+//			
+// Author:          Erin Hamilton
+// Email:           erin@erinhamilton.me
+//
+// Last Updated:    December 2013
+//
+// Description:     Controls main functionality of Wisconsin Glacial Extents.
+//                  Calls functions to create map, timeline, and dialog on 
+//                  page load.
+//                   
+// Credits:         Funding provided by Great Lakes Mapping Coalition
+//////////////////////////// 80 columns wide //////////////////////////////////
+
+/** Global Variables*/
+//Years array. Change the years in this array if new dates are added or taken away. 
+var years = ["31500", "31000", "30000", "29500", "29000", "28500", "28000", "27500", "27000", "26500", "26000",
+"25500", "25000", "24500", "24000", "23500", "23000", "22500", "22000", "21500", "21000", "20500",
+"20000", "19500", "19000", "18500", "18000", "17000", "16600", "16000", "15750", "15500", "15250", 
+"15000", "14600", "14000", "13500", "13250", "13100", "12900", "12500", "12000", "11500", "11000"];
+
+var lakeYears = ['-31500', '-31000', '-30000', '-29500', '-29000', '-28500', '-28000', '-27500', '-27000', '-26500', '-26000',
+'-25500', '-25000', '-24500', '-24000', '-23500', '-23000', '-22500', '-22000', '-21500', '-21000', '-20500',
+'-20000', '-19500', '-19000', '-18500', '-18000', '-17000', '-16600', '-16000', '-15750', '-15500', '-15250', 
+'-15000', '-14600', '-14000', '-13500', '-13250', '-13100', '-12900', '-12500', '-12000', '-11500', '-11000'];
+
+var yearText = (function () {
+    var yearText = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': '../glacial/data/yearWords.json',
+        'dataType': "json",
+        'success': function (data) {
+            yearText = data;
+        }
+    });
+    return yearText;
+})();
+
+
+
+/**
+ * On page load, call functions to create map, timeline, and dialog.
+ */
+function initialize() {
+	"use strict";
+	setMap();
+	
+}
+
+
+/**
+ * After page finishes loading, jQuery call to function initialize.
+ */
+$(initialize);
+
+// 
---
+++ js/map.js
@@ -0,0 +1,81 @@
+///////////////////////////////////////////////////////////////////////////////
+// Main Class File:   main.js, leaflet.v0.5.1.js
+// File:              map.js
+//
+// Author:          Erin Hamilton
+// Email:           erin@erinhamilton.me
+//
+// Last Updated:    December 2013
+//
+// Description:     Creates the Leaflet map displaying the glacial extents in
+//                  Wisconsin. Fetches data from ArcGIS server asynchronously.
+//                   
+// Credits:         Funding provided by Great Lakes Mapping Coalition
+//////////////////////////// 80 columns wide //////////////////////////////////
+
+/**Global Variabls**/
+var map;
+var glaciers;
+var lakes;
+var states;
+
+/**
+ * Creates the map from leaflet and sets the view extent and tilelayer source.
+ * Called from main.js initialize()
+ */
+function setMap(){
+	var zoom = 6;
+	var center = [44.25, -88.75];
+
+	var acetate = L.tileLayer(
+		'http://{s}.acetate.geoiq.com/tiles/acetate/{z}/{x}/{y}.png',	
+		{
+			attribution: 'Acetate tileset from GeoIQ'
+		});
+
+
+	var wgnhs = L.tileLayer.wms(
+		//'http://gis.wgnhs.org/arcgis/services/dev/glacial_extents_topo/MapServer/WMSServer',
+		'http://cyclone.ad.wgnhs.uwex.edu/arcgis/services/dev/glacial_extents_topo/MapServer/WMSServer',
+		
+		{
+			format: 'image/png',
+			layers: 1,
+			transparent: true,
+			opacity: 0.7,
+			attribution: 'Wisconsin Geological and Natural History Survey tiles'
+		});	
+		
+		var wgnhsShade = L.tileLayer.wms(
+		//'http://gis.wgnhs.org/arcgis/services/dev/glacial_extents_topo/MapServer/WMSServer',
+		'http://cyclone.ad.wgnhs.uwex.edu/arcgis/services/dev/glacial_extents_topo/MapServer/WMSServer',
+		
+		{
+			format: 'image/png',
+			layers: 0,
+			attribution: 'Wisconsin Geological and Natural History Survey tiles'
+		});	
+		
+		var baseMaps = {
+			"Topography": wgnhs,
+			"Streets": acetate
+		};
+		
+		map = L.map('map', {
+		center: center,
+		zoom: zoom,
+		maxZoom: zoom,
+		minZoom: zoom,
+		dragging: false,
+		zoomControl: false,
+		keyboard: false,
+		doubleClickZoom: false,
+		layers: [wgnhs, wgnhsShade]
+	});
+		
+	var baseLayerControl =   L.control.layers(baseMaps, null, {position: 'topleft', collapsed: true});
+
+	baseLayerControl.addTo(map);
+	
+	
+}
